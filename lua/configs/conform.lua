8e5ec5151c0971239d98aee2b2e4dfb138f169a6
Florian Ruechel <code@inexplicity.de>
2025-11-28 23:53:29 +1030

    Add prettier to conform

--- lua/configs/conform.lua
+++ lua/configs/conform.lua
@@ -10,6 +10,7 @@ return {
     markdown = { "mdformat" },
     ["terraform-vars"] = { "terraform_fmt" },
     typescript = { "prettier" },
+    yaml = { "prettier" },
   },
   format_on_save = { timeout_ms = 500, lsp_fallback = true },
   formatters = {
