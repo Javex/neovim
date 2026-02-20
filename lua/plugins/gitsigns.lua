da152b398238885ff081be09f654f84f0fa30579
Florian Ruechel <code@inexplicity.de>
2026-02-20 22:07:16 +1030

    Remove event because gitsigns wasn't loading

--- lua/plugins/gitsigns.lua
+++ lua/plugins/gitsigns.lua
@@ -1,8 +1,6 @@
 return { -- Adds git related signs to the gutter, as well as utilities for managing changes
   'lewis6991/gitsigns.nvim',
 
-  -- event is copied from NvChad, not sure if necessary
-  event = 'User FilePost',
   opts = {
     signs = {
       add = { text = '+' },
