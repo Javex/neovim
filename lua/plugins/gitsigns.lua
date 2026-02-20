37bb920ebc01f76f2ad583a0a42224eb3ee32beb
Florian Ruechel <code@inexplicity.de>
2026-02-20 20:40:35 +1030

    Configure & clean up gitsigns

--- lua/plugins/gitsigns.lua
+++ lua/plugins/gitsigns.lua
@@ -1,21 +1,8 @@
-return
--- Alternatively, use `config = function() ... end` for full control over the configuration.
--- If you prefer to call `setup` explicitly, use:
---    {
---        'lewis6991/gitsigns.nvim',
---        config = function()
---            require('gitsigns').setup({
---                -- Your gitsigns configuration here
---            })
---        end,
---    }
---
--- Here is a more advanced example where we pass configuration
--- options to `gitsigns.nvim`.
---
--- See `:help gitsigns` to understand what the configuration keys do
-{ -- Adds git related signs to the gutter, as well as utilities for managing changes
+return { -- Adds git related signs to the gutter, as well as utilities for managing changes
   'lewis6991/gitsigns.nvim',
+
+  -- event is copied from NvChad, not sure if necessary
+  event = 'User FilePost',
   opts = {
     signs = {
       add = { text = '+' },
